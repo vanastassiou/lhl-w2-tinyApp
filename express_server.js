878601d60a3718839312b8538f98bc719a702574
vanastassiou <v.anastassiou@gmail.com>
2017-11-08 22:18:33 +0000

    Corrected variable names to align with convention

--- express_server.js
+++ express_server.js
@@ -1,48 +1,48 @@
 const express = require("express");
-const expressAction = express();
+const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 
-expressAction.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-expressAction.set("view engine", "ejs")
+app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-expressAction.get("/", (request, response) => {
-  response.end("This is the root directory of the website! We actually don't want you to be here. You should go elsewhere...BUT WHERE??");
+app.get("/", (req, res) => {
+  res.end("This is the root directory of the website! We actually don't want you to be here. You should go elsewhere...BUT WHERE??");
 });
 
-expressAction.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`TinyURL clone app listening on port ${PORT}!`);
 });
 
 // Spits out urlDatabase object contents in JSON format.
-expressAction.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 // Congratulatons! You just made an API!
 
-expressAction.get("/urls", (request, response) => {
+app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
-  response.render("urls_index", templateVars);
+  res.render("urls_index", templateVars);
 })
 
-expressAction.get("/urls/:id", (request, response) => {
-  let templateVars = { shortURL: request.params.id };
-  response.render("urls_show", templateVars);
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
 });
 
-expressAction.get("/urls/new", (request, response) => {
-  response.render("urls_new");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
-expressAction.post("/urls", (request, response) => {
-  console.log(request.body);  // debug statement to see POST parameters
-  response.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 function generateRandomString() {
@@ -50,6 +50,6 @@ function generateRandomString() {
   return result;
 }
 
-expressAction.get("/ohai", (request, response) => {
-  response.end("<html><body>OMGOMGOMGOMG I'M IN SPACE</body></html>\n");
+app.get("/ohai", (req, res) => {
+  res.end("<html><body>OMGOMGOMGOMG I'M IN SPACE</body></html>\n");
 });
